467f53f8ee97e4623996737df9610938800d8a19
CondesnsedMilk <achau103@student.hbuhsd.edu>
2025-01-14 21:49:13 +0000

    MinList Incomplete

--- Program.cs
+++ Program.cs
@@ -1,24 +1,31 @@
-﻿namespace unit_4_6_assignment_a_CondesnsedMilk;
+﻿using System.Globalization;
+
+namespace unit_4_6_assignment_a_CondesnsedMilk;
 
 class Program
 {
     static void Main(string[] args)
     {
-        QuadForm(5, 9, 14);
-    }
-    static void QuadForm(double a, double b, double c)
-    {
-        double x1 = -b + Math.Sqrt(Math.Pow(b,2) - 4 * a * c);
-        double x2 = -b - Math.Sqrt(Math.Pow(b,2) - 4 * a * c);
         
-        if (!double.IsNaN(x1) || double.IsPositiveInfinity(x1) || double.IsNegativeInfinity(x1))
+    }
+    static void MinList(float a)
+    {
+        var listA = new List<int>() { 1, 2, 3, 4 };
+        var listB = new List<int>() {3, 2, 1, 0};
+        var listC = new List<int>() {-1, -5, -5, -3};
+
+        int count = 0;
+        int minNum;
+
+        while (count < listA.Count)
         {
-            Console.WriteLine($"Your answers are {x1/2} and {x2/2}");
+            if ()
+            {
+                listA.GetRange[];
+            }
+            count++;
         }
-        else
-        {
-            Console.WriteLine("There are no x intercepts");
-        }
-            
+
+        Console.WriteLine($"The minimum is {minNum}");
     }
 }
